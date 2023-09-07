00fa70b939c8fe13cc2e33011ec5c8bd965d246b
Kavitha-Beemaraj <117799487+Kavitha-Beemaraj@users.noreply.github.com>
2023-09-07 19:37:51 +0530

    update

--- server.js
+++ server.js
@@ -4,31 +4,31 @@ const app= express()
 app.get('/add', (req,res) => {
     logInput(req.query)
     const {num1,num2}=(req.query)
-    res.send(`answer is ${parseInt(num1)+parseInt(num2)}`)
+    res.send(`addition answer is ${parseInt(num1)+parseInt(num2)}`)
 })
 
 app.get('/sub', (req,res) => {
     logInput(req.query)
     const {num1,num2}=(req.query)
-    res.send(`answer is ${parseInt(num1)+parseInt(num2)}`)
+    res.send(`subraction answer is ${parseInt(num1)+parseInt(num2)}`)
 })
 
 app.get('/multiply', (req,res) => {
     logInput(req.query)
     const {num1,num2}=(req.query)
-    res.send(`answer is ${parseInt(num1)+parseInt(num2)}`)
+    res.send(`multiplication answer is ${parseInt(num1)+parseInt(num2)}`)
 })
 
 app.get('/divide', (req,res) => {
     logInput(req.query)
     const {num1,num2}=(req.query)
-    res.send(`answer is ${parseInt(num1)+parseInt(num2)}`)
+    res.send(`divide answer is ${parseInt(num1)+parseInt(num2)}`)
 })
 
 app.listen(4000);
 
 function logInput(query){
-    const {num1,num2}=(req.query)
+    const {num1,num2}=(query)
     console.log('num1 is', num1)
     console.log('num2 is', num2)
 }
